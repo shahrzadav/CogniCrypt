3cb968e7c216df53addcdc5977b550f593a5d048
Shahrzad <shahrzad.asghari73@gmail.com>
2019-11-06 13:53:04 +0100

    resizing wondow for expandable composite in progress.
    Signed-off-by: Shahrzad <shahrzad.asghari73@gmail.com>

--- plugins/de.cognicrypt.core/src/de/cognicrypt/core/properties/CogniCryptPreferencePage.java
+++ plugins/de.cognicrypt.core/src/de/cognicrypt/core/properties/CogniCryptPreferencePage.java
@@ -16,13 +16,20 @@ import org.eclipse.core.runtime.preferences.InstanceScope;
 import org.eclipse.jface.preference.IPreferenceStore;
 import org.eclipse.jface.preference.PreferencePage;
 import org.eclipse.swt.SWT;
+import org.eclipse.swt.events.ExpandListener;
+import org.eclipse.swt.layout.GridData;
 import org.eclipse.swt.layout.GridLayout;
+import org.eclipse.swt.layout.RowData;
 import org.eclipse.swt.layout.RowLayout;
 import org.eclipse.swt.widgets.Composite;
 import org.eclipse.swt.widgets.Control;
+import org.eclipse.swt.widgets.ExpandItem;
 import org.eclipse.swt.widgets.Label;
 import org.eclipse.ui.IWorkbench;
 import org.eclipse.ui.IWorkbenchPreferencePage;
+import org.eclipse.ui.forms.events.ExpansionAdapter;
+import org.eclipse.ui.forms.events.ExpansionEvent;
+import org.eclipse.ui.forms.events.IExpansionListener;
 import org.eclipse.ui.forms.widgets.ExpandableComposite;
 import org.osgi.service.prefs.BackingStoreException;
 import de.cognicrypt.core.Activator;
@@ -37,18 +44,30 @@ public class CogniCryptPreferencePage extends PreferencePage implements IWorkben
 	@Override
 	protected Control createContents(Composite parent) {
 		final Composite container = new Composite(parent, SWT.FILL);
+		container.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		container.setLayout(new GridLayout(1, true));
 		notifyBasicPreferenceListeners(container);
-
+		
 		new Label(container, SWT.NONE);
 		final ExpandableComposite collap = new ExpandableComposite(container, SWT.Collapse);
 		collap.setText("Advanced Options");
 
 		final Composite advancedOptions = new Composite(collap, SWT.None);
 		collap.setClient(advancedOptions);
+		collap.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
+		advancedOptions.setLayoutData(new GridData(SWT.FILL, SWT.FILL, true, true));
 		advancedOptions.setLayout(new RowLayout(SWT.VERTICAL));
-		notifyAdvancedPreferenceListeners(advancedOptions);
 
+		notifyAdvancedPreferenceListeners(advancedOptions);
+		
+		collap.addExpansionListener(new ExpansionAdapter() {
+			public void expansionStateChanged(ExpansionEvent e) {
+//				System.out.println("SIZE OF COLLAP : " + collap.getSize());
+//				System.out.println("SIZE OF whole : " + container.getSize());
+				container.layout(true);
+
+			}
+		});
 		return container;
 	}
 
