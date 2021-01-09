15b288f8a3ef710830473aca4036cfdba8fd7639
Shahrzad <shahrzad.asghari73@gmail.com>
2021-01-09 17:37:27 +0100

    added hash and salt, authentication works.
    Signed-off-by: Shahrzad <shahrzad.asghari73@gmail.com>

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/crysl/templates/userauthenticationauth/UserAuthentication.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/crysl/templates/userauthenticationauth/UserAuthentication.java
@@ -1,43 +1,43 @@
 package de.cognicrypt.codegenerator.crysl.templates.userauthenticationauth;
 
 
+import java.nio.charset.StandardCharsets;
+import java.security.MessageDigest;
 import java.security.NoSuchAlgorithmException;
+import java.security.SecureRandom;
 import java.security.spec.InvalidKeySpecException;
+import java.security.spec.KeySpec;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.Arrays;
+import java.util.Base64;
+
+import javax.crypto.SecretKeyFactory;
+import javax.crypto.spec.PBEKeySpec;
 
 import de.cognicrypt.codegenerator.crysl.CrySLCodeGenerator;
 
 
 public class UserAuthentication {
-	
-//	static Scanner sc = new Scanner(System.in);
-	//for this to work, sql must be installed and put in path
-	@SuppressWarnings("null")
-	public static void userAuth(String username, String pwd) throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
+	//hash and salt will be stored as string
+	//for this to work, sql must be installed and put in env path
+	public static void userAuth(String username, char[] pwd) throws SQLException, NoSuchAlgorithmException, InvalidKeySpecException {
 	    String databaseUsername = "root";
 	    String databasePassword = "test";
-	    String databaseURL = "jdbc:mysql://localhost:3306/mydb";
+	    String databaseURL = "jdbc:mysql://localhost:3306/myDatabase";
 	    String tableName = " mytb ";
 	    
 	    String usernameInDB = "";
-	    String passwordInDB = "";
+	    String salt = "" ;
+	    String hash = "";
+	    
 	    int iterationCount = 65536;
 	    int keysize = 128;
 	    
-	    byte[] salt = new byte[32];
-	    
-	    Object encryptionKey = null;
-	    
-	    char[] charPwd = pwd.toCharArray();
-	    
-		CrySLCodeGenerator.getInstance().includeClass("java.security.SecureRandom").addParameter(salt, "next").includeClass("javax.crypto.spec.PBEKeySpec")
-		.addParameter(charPwd, "password").addParameter(keysize, "keylength").addParameter(iterationCount, "iterationCount").includeClass("javax.crypto.SecretKeyFactory").includeClass("javax.crypto.SecretKey")
-		.addParameter(encryptionKey, "this").generate();
-		
+	    byte[] hashedPass = null;
 	    //this may cause error when mysql is not in the path
 	    try {
 			Class.forName("com.mysql.cj.jdbc.Driver");
@@ -47,18 +47,27 @@ public class UserAuthentication {
 		}  
 		Connection connection=DriverManager.getConnection(databaseURL, databaseUsername, databasePassword);  
 
-	    // Create SQL Query
 	    Statement stmt = connection.createStatement();
-	    String SQL = "SELECT * FROM" + tableName + "WHERE USERNAME='" + username + "' && PASSWORD='" + pwd + "'";
-
+	    String SQL = "SELECT * FROM" + tableName + "WHERE USERNAME='" + username + "'";
 	    ResultSet queryResult = stmt.executeQuery(SQL);
-	     // Check Username and Password
 	    while (queryResult.next()) {
 	    	usernameInDB = queryResult.getString("username");
-	    	passwordInDB = queryResult.getString("password");
+	    	salt = queryResult.getString("salt");
+	    	hash = queryResult.getString("hash");
+	    	
 	    }
 
-	    if (username.equals(usernameInDB) && encryptionKey.equals(passwordInDB)) {
+	    byte [] bytedSalt = Base64.getDecoder().decode(salt);
+		
+		CrySLCodeGenerator.getInstance().includeClass("javax.crypto.spec.PBEKeySpec")
+		.addParameter(pwd, "password").addParameter(keysize, "keylength").addParameter(iterationCount, "iterationCount")
+		.addParameter(bytedSalt, "salt").includeClass("javax.crypto.SecretKeyFactory").includeClass("javax.crypto.SecretKey")
+		.addParameter(hashedPass, "this").generate();
+
+		String hashedPassString = Base64.getEncoder().encodeToString(hashedPass);
+
+	    if (username.equals(usernameInDB) && hash.equals(hashedPassString)) {
+	    	
 	        System.out.println("Successful Login!\n----");
 	    } else {
 	        System.out.println("Incorrect Password\n----");
@@ -66,7 +75,9 @@ public class UserAuthentication {
 	}
 	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
 		try {
-			userAuth("test", "test");
+			String pass = "non";
+			char[] charPass = pass.toCharArray();
+			userAuth("test", charPass);
 		} catch (SQLException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
