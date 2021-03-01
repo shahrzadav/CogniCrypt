4d2b361ed7f195ead1413d3ab345797b78889db7
Shahrzad <shahrzad.asghari73@gmail.com>
2021-03-01 12:26:59 +0100

    added javadocs to new templates.
    Signed-off-by: Shahrzad <shahrzad.asghari73@gmail.com>

--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/crysl/templates/userauthenticationauth/UserAuthentication.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/crysl/templates/userauthenticationauth/UserAuthentication.java
@@ -1,26 +1,51 @@
+/********************************************************************************
+ * Copyright (c) 2015-2021 TU Darmstadt, Paderborn University
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
 package de.cognicrypt.codegenerator.crysl.templates.userauthenticationauth;
 
-import java.security.NoSuchAlgorithmException;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.sql.Statement;
 import java.util.Base64;
 
 import de.cognicrypt.codegenerator.crysl.CrySLCodeGenerator;
 
-
+/**
+ * The Class UserAuthentication verifies the users input, the username and password.
+ */
 public class UserAuthentication {
 	//hash and salt will be stored as string
+	/**
+	 * Takes username and password as inputs to verifies the password. Connects to the MySQL database
+	 * and retrieves the hashed password and salt for the desired username and makes a new hash from the input password
+	 * with the same salt, then checks the equality of the hash in the database and the new hash. If the username does not 
+	 * exist in the database, it throws an exception. Users may modify the database's username, password, URL and tableName to their own.
+	 * Usernames and Passwords are all considered to be unique.
+	 *
+	 * @param username the input username to be searched in the database.
+	 * @param pwd the input password for that username, the correction of this password will be checked by this method.
+	 * @return true, if the username exists and the input password is correct.
+	 * @throws Exception This exception is thrown if the username does not exist in the database.
+	 * @throws ClassNotFoundException This exception is thrown if MySQL jar file is not in the buildpath. It contains the class "com.mysql.cj.jdbc.Driver".
+	 * @throws GeneralSecurityException  This exception is thrown if a security-related exception happens that extends this general exception.
+	 * @throws InvalidKeySpecException This exception is thrown when key specifications are invalid.
+	 */
 	//for this to work, sql must be installed and put in env path
-	public static void userAuth(String username, char[] pwd) throws SQLException, NoSuchAlgorithmException {
+	public static boolean userAuth(String username, char[] pwd) throws Exception {
 	    String databaseUsername = "root";
 	    String databasePassword = "test";
 	    String databaseURL = "jdbc:mysql://localhost:3306/myDatabase";
 	    String tableName = " mytb ";
 	    
-	    String usernameInDB = "";
+	    Boolean result = false;
+	    
 	    String salt = "" ;
 	    String hash = "";
 	    
@@ -28,23 +53,21 @@ public class UserAuthentication {
 	    int keysize = 128;
 	    
 	    byte[] hashedPwd = null;
-	    //this may cause error when mysql is not in the path
-	    try {
-			Class.forName("com.mysql.cj.jdbc.Driver");
-		} catch (ClassNotFoundException e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}  
-		Connection connection=DriverManager.getConnection(databaseURL, databaseUsername, databasePassword);  
+
+		Class.forName("com.mysql.cj.jdbc.Driver");
+	 
+		Connection connection = DriverManager.getConnection(databaseURL, databaseUsername, databasePassword);  
 
 	    Statement stmt = connection.createStatement();
 	    String SQL = "SELECT * FROM" + tableName + "WHERE USERNAME='" + username + "'";
 	    ResultSet queryResult = stmt.executeQuery(SQL);
-	    while (queryResult.next()) {
-	    	usernameInDB = queryResult.getString("username");
-	    	salt = queryResult.getString("salt");
-	    	hash = queryResult.getString("hash");
-	    	
+	    if (queryResult.isBeforeFirst()) {
+		    while (queryResult.next()) {
+		    	salt = queryResult.getString("salt");
+		    	hash = queryResult.getString("hash");
+		    }
+	    }else {
+	    	throw new Exception("User not found");
 	    }
 
 	    byte [] bytedSalt = Base64.getDecoder().decode(salt);
@@ -56,21 +79,8 @@ public class UserAuthentication {
 
 		String hashedPwdString = Base64.getEncoder().encodeToString(hashedPwd);
 
-	    if (username.equals(usernameInDB) && hash.equals(hashedPwdString)) {
-	    	
-	        System.out.println("Successful Login!\n----");
-	    } else {
-	        System.out.println("Incorrect Password\n----");
-	    }
+	    if (hash.equals(hashedPwdString)) {
+	        result = true;}
+	    return result;
 	}
-//	public static void main(String[] args) throws NoSuchAlgorithmException, InvalidKeySpecException {
-//		try {
-//			String pass = "non";
-//			char[] charPass = pass.toCharArray();
-//			userAuth("test", charPass);
-//		} catch (SQLException e) {
-//			// TODO Auto-generated catch block
-//			e.printStackTrace();
-//		}
-//	}
 }
--- plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/crysl/templates/userauthenticationpassgen/PassGenerator.java
+++ plugins/de.cognicrypt.codegenerator/src/main/java/de/cognicrypt/codegenerator/crysl/templates/userauthenticationpassgen/PassGenerator.java
@@ -1,9 +1,29 @@
+/********************************************************************************
+ * Copyright (c) 2015-2021 TU Darmstadt, Paderborn University
+ * 
+ * This program and the accompanying materials are made available under the
+ * terms of the Eclipse Public License v. 2.0 which is available at
+ * http://www.eclipse.org/legal/epl-2.0.
+ * 
+ * SPDX-License-Identifier: EPL-2.0
+ ********************************************************************************/
+
 package de.cognicrypt.codegenerator.crysl.templates.userauthenticationpassgen;
 
 import de.cognicrypt.codegenerator.crysl.CrySLCodeGenerator;
 
+/**
+ * The Class PassGenerator, generates a secure password.
+ */
 public class PassGenerator {
 	
+	/**
+	 * Generates a password using Passay library with 8 characters. It involves
+	 * 2 of each set of characters, capital case letters, lower case letters,
+	 * digits and special characters, which are distributed randomly in the password.
+	 *
+	 * @return the password.
+	 */
 	public static String generatePassayPassword() {
 		int length = 8;
 	   
@@ -22,6 +42,13 @@ public class PassGenerator {
 	  return res;
 	}
 
+	/**
+	 * Defines a set of character and a number of that character in the password.
+	 *
+	 * @param characters the characters, digits, letters, etc.
+	 * @param amount the amount of those characters in the password.
+	 * @return the character rule.
+	 */
 	public static org.passay.CharacterRule generateRule(org.passay.CharacterData characters, int amount) {
 		org.passay.CharacterRule charRule = new org.passay.CharacterRule(characters);
 		charRule.setNumberOfCharacters(amount);
